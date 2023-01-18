6d57cec5ba26b5691e49e41e57b006ca5d49e916
myjyby <myjyby@gmail.com>
2023-01-18 22:17:47 +0100

    configured for experimenters platform

--- config/edit/index.js
+++ config/edit/index.js
@@ -17,10 +17,10 @@ exports.app_description = {
 exports.modules = [
 	{ type: 'pads', rights: { read: 0, write: 1 } }, 
 	{ type: 'pinboards', rights: { read: 0, write: 1 } },
-	{ type: 'templates', rights: { read: 2, write: 2 } },
+	{ type: 'templates', rights: { read: 2, write: 3 } },
 	// { type: 'files', rights: { read: 0, write: 1 } }, 
-	// { type: 'reviews', rights: { read: 1, write: 1, coordinate: 3 }, reviewers: 2 }, // TO DO: UPDATE THIS TO 2
-	{ type: 'mobilizations', rights: { read: 2, write: 2 } }, 
+	// { type: 'reviews', rights: { read: 1, write: 1, coordinate: 3 }, reviewers: 2 },
+	// { type: 'mobilizations', rights: { read: 2, write: 2 } }, 
 	{ type: 'contributors', rights: { read: 2, write: 2 } }, 
 	{ type: 'teams', rights: { read: 2, write: 2 } }
 	
@@ -39,11 +39,6 @@ exports.metafields = [
 	{ type: 'tag', name: 'datasources', required: true, opencode: true },
 	
 	// { type: 'location', name: 'locations', required: true },
-	// { type: 'attachment', name: 'consent', required: true, uris: [ { uri: 'https://acclabs-consent-archive.azurewebsites.net/api/join/file' }, { uri: undefined } ] }, // THIS IS FOR CONSENT FORMS. A DOCUMENT CAN COME FROM THE CONTEXT, OR BE AN EMBEDED LINK
-	// { type: 'attachment', name: 'consent', required: true, uris: [ { uri: 'http://localhost:3000/api/join/file' }, { uri: undefined } ], limit: 1 } // THIS IS FOR CONSENT FORMS. A DOCUMENT CAN COME FROM THE CONTEXT, OR BE AN EMBEDED LINK
-	
-	// { type: 'txt', name: 'mapper name', required: true } // THIS IS A SIMPLE TEST OF ANOTHER TYPE OF MEDIA (txt)
-	
 
 	{ type: 'radiolist', name: 'experiment status', instruction: 'Current status of experimental activity',
 	options: [

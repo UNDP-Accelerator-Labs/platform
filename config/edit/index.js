c0dcceb7196360ac2f4fa4220a64c154c152b63b
myjyby <myjyby@gmail.com>
2023-01-20 10:45:19 +0100

    tweaked css for title

--- config/edit/index.js
+++ config/edit/index.js
@@ -4,13 +4,7 @@ exports.app_title = 'UNDP AccLabs Pads, Generic pads'
 exports.app_title_short = 'pads'
 exports.app_suite = 'acclab_platform'
 exports.app_languages = ['en', 'fr', 'es', 'pt']
-exports.app_description = {
-	'en': 'This is a description of the app.',
-	// 'en': '<br/><br/>Intriguing solutions are those that, when observed, make you wonder ‘why did they do that?’. You see something happening, but you don’t immediately understand the need behind it. These are solutions that get a mapper to investigate further and dig deeper to understand why.',
-	'fr': 'Ceci est une description de l’appli.',
-	'es': 'This is a description of the app.',
-	'pt': 'This is a description of the app.',
-}
+exports.app_description = require('./translations.js').app_description
 
 
 // DESIRED MODULES

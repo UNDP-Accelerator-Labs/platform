fdcd92deca0bf566d1ebeaefba04320633e9e3f8
myjyby <myjyby@gmail.com>
2023-02-21 09:55:51 +0100

    fixed templates for Edu

--- routes/browse/templates/load/filters_menu.js
+++ routes/browse/templates/load/filters_menu.js
@@ -22,11 +22,13 @@ exports.main = async kwargs => {
 			const batch1 = []
 			if (space === 'private') {
 				batch1.push(t1.any(`
-					SELECT COUNT (DISTINCT (id))::INT, owner
-					FROM templates
-					WHERE owner IN ($1:csv)
+					SELECT COUNT (DISTINCT (t.id))::INT, t.owner
+					FROM templates t
+					-- WHERE owner IN ($1:csv)
+					WHERE TRUE
+						$1:raw
 					GROUP BY owner
-				;`, [ req.session.collaborators.map(d => d.uuid) ])
+				;`, [ full_filters ])//[ req.session.collaborators.map(d => d.uuid) ])
 				.then(async results => {
 					let contributors = await join.users(results, [ language, 'owner' ])
 					// THIS NEEDS SOME CLEANING FOR THE FRONTEND

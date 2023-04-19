1bda6664123f5f716cbd5e6698367a8fbfaf0af2
myjyby <myjyby@gmail.com>
2023-04-19 14:54:23 +0200

    testing the session sharing

--- routes/login/check.js
+++ routes/login/check.js
@@ -1,3 +1,4 @@
+const { app_suite } = include('config/')
 const { datastructures } = include('routes/helpers/')
 const processlogin = require('./process.js')
 const jwt = require('jsonwebtoken')
@@ -6,6 +7,13 @@ module.exports = (req, res, next) => {
 	const token = req.body.token || req.query.token || req.headers['x-access-token']
 	const { uuid } = req.session || {}
 	
+	const cookies = parseCookies(req)
+	let sid = cookies[`${app_suite}-session`]
+	if (sid) sid = sid.split(':')[1].split('.')[0]
+	console.log(cookies)
+	console.log(sid)
+
+
 	if (uuid) next() // A USER IS LOGGED
 	else if (token) processlogin(req, res) // A LOGIN TOKEN IS RECEIVED
 	else { 
@@ -13,4 +21,23 @@ module.exports = (req, res, next) => {
 		Object.assign(req.session, datastructures.sessiondata({ public: true }))
 		next()
 	}
+}
+
+// CREDIT TO https://stackoverflow.com/questions/3393854/get-and-set-a-single-cookie-with-node-js-http-server
+function parseCookies (req) {
+	const list = {}
+	const cookieHeader = req.headers?.cookie
+	if (!cookieHeader) return list
+
+	cookieHeader.split(';')
+	.forEach(cookie => {
+		let [ name, ...rest] = cookie.split('=')
+		name = name?.trim()
+		if (!name) return
+		const value = rest.join('=').trim()
+		if (!value) return
+		list[name] = decodeURIComponent(value)
+	})
+
+	return list
 }

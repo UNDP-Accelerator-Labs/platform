d7106f43c4497665cc714b001f7ed45348a5d836
Josua Krause <josua.krause@gmail.com>
2023-06-07 14:19:15 -0400

    bring over sh fixes

--- sh/run.sh
+++ sh/run.sh
@@ -2,11 +2,11 @@
 
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 
-if [ -z "$CMD" ]; then
+if [ -z "${CMD}" ]; then
     echo "specify a command via CMD"
     exit 1
 fi
 
 source ./sh/env.sh
 
-npm run $CMD $@
+npm run ${CMD} "$@"

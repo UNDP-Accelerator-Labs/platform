11429d454aab32f19a80a7bf015689e383fd6033
myjyby <myjyby@gmail.com>
2023-09-28 15:52:25 +0200

    some more very minor tweaks

--- routes/helpers/rights/index.js
+++ routes/helpers/rights/index.js
@@ -4,11 +4,12 @@ module.exports = async (_kwargs) => {
 	const conn = _kwargs.connection || DB.general
 	const { uuid } = _kwargs
 
-	const right = await conn.oneOrNone(`
-		SELECT COALESCE(rights, 0)::INT AS rights FROM users
-		WHERE uuid = $1
-	;`, [ uuid ], d => d.rights)
-
-	// const rights = right?.rights ?? 0;
-    return right;
+	let rights = 0;
+	if (uuid) {
+		rights = await conn.oneOrNone(`
+			SELECT COALESCE(rights, 0)::INT AS rights FROM users
+			WHERE uuid = $1
+		;`, [ uuid ], d => d.rights);
+	}
+    return rights;
 }

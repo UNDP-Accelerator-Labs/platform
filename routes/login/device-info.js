97e9e548b8889646b92050663b8408080dc380ce
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-10-18 12:30:56 +0100

    Make reset password link clickable in email

--- routes/login/device-info.js
+++ routes/login/device-info.js
@@ -13,12 +13,15 @@ exports.deviceInfo = (req) => {
 exports.sendDeviceCode = (_kwarg) => {
   const { email : sendEmail } = include('routes/helpers/')
 
-  const { conn, uuid, email, name } = _kwarg;
+  const { conn, uuid, email, name, req } = _kwarg;
   let code = 0;
   while (`${code}`.length < 6) {
     code += Math.floor(Math.random() * 1000000);
     code %= 1000000;
   }
+
+  const { protocol, host } = req || {}
+  const resetLink = `${protocol}://${host}/forget-password`;
 
   // Save the code to the database
   return conn
@@ -42,7 +45,7 @@ exports.sendDeviceCode = (_kwarg) => {
           <p>Please confirm this device using the OTP (One Time Password) below:</p>
           <h3>${code}</h3>
           <p>This OTP will expire in 10 minutes.</p>
-          <p>If you are receiving this email and did not initiate this request, your account might be compromised. Please reset your password or contact system administrators.</p>
+          <p>If you are receiving this email and did not initiate this request, your account might be compromised. Please <a href="${resetLink}">reset your password</a> or contact system administrators.</p>
           <br/>
           <p>Best regards,</p>
           <p>AccLab Global Team</p>

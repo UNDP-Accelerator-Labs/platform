5f7181bb823c9ff102bcc3edeca23e174d94ca93
Josua Krause <josua.krause@gmail.com>
2023-11-14 15:08:06 -0500

    use service

--- routes/helpers/email/index.js
+++ routes/helpers/email/index.js
@@ -14,7 +14,7 @@ module.exports = async (kwargs) => {
 	let transporter = nodeMailer.createTransport({
 		host: process.env.SMTP_HOST,
 		port: process.env.SMTP_PORT,
-		secure: true,
+		service: process.env.SMTP_SERVICE,
 		auth: {
 			user: process.env.SMTP_USER,
 			pass: process.env.SMTP_PASSWD,

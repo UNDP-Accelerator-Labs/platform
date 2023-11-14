137987d3a267527b59119202b75e5994043e44c9
Josua Krause <josua.krause@gmail.com>
2023-11-14 13:56:05 -0500

    log email send

--- routes/helpers/email/index.js
+++ routes/helpers/email/index.js
@@ -27,6 +27,7 @@ module.exports = async (kwargs) => {
 		html,
 	};
 
+	console.log('SENDING EMAIL', from, to, subject);
 	return new Promise(resolve => {
 		transporter.sendMail(mailOptions, (err, info) => {
 			if (err) resolve({ status: 500, message: err })

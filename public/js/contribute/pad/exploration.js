9b21a221d8353944b61dc7ea78cd986a781c12aa
myjyby <myjyby@gmail.com>
2023-12-04 11:48:18 +0100

    fixed adjacent view

---
+++ public/js/contribute/pad/exploration.js
@@ -0,0 +1,15 @@
+// INIT EXPLORATION
+function initExploration () {
+	const exploration = new Exploration();
+
+	exploration.setUseFullPromptForSelect(true);
+	exploration.updateExplorationList(() => {
+		const docBtn = d3.select('div.exploration-local');
+		const data = { id: pad.id };
+		if (data.id) {
+			docBtn.data([data]);
+			exploration.setVisible(true);
+			exploration.addDocButtons(docBtn, false);
+		}
+	});
+}

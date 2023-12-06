fb0c8d179906215670cd2346309277fa549b108c
Josua Krause <josua.krause@gmail.com>
2023-12-06 16:53:00 -0500

    better sw

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ const cookieParser = require('cookie-parser');
 const swPrecache = require('sw-precache');
 swPrecache.write('./public/app.serviceWorker.js', {
   root: './public/',
-  staticFileGlobs: ['./public/**/*'],
+  staticFileGlobs: [
+    './public/css/**/*',
+    './public/imgs/**/*',
+    './public/js/**/*',
+    './public/favicon.ico',
+  ],
   stripPrefix: './public/',
   maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
 });

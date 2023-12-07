e2f691618ed160111018a3faae23db937ede7d53
myjyby <myjyby@gmail.com>
2023-12-07 11:40:14 +0100

    should be fully fixed now

--- public/js/contribute/resource/load.js
+++ public/js/contribute/resource/load.js
@@ -21,7 +21,7 @@ async function DOMLoad() {
   const template_opts = {};
 
   resources.forEach((key) => {
-    if (readables[key].length) {
+    if (readables?.[key]?.length) {
       module_opts[key] = readables[key].map((d) => {
         return {
           label: `${d.title || d.name}${
@@ -57,135 +57,140 @@ async function DOMLoad() {
           });
         },
       });
-      if (['templates', 'pads'].includes(key) && writables[key]) {
-        opts.push({
-          node: 'div',
-          class: 'divider',
-          label: vocabulary['or'].toUpperCase(),
+      if (key !== Object.keys(readables)[Object.keys(readables).length - 1]) {
+        if (opts.length && opts[opts.length - 1]?.class !== 'divider') {
+	        opts.push({
+	          node: 'div',
+	          class: 'divider',
+	          label: vocabulary['or'].toUpperCase(),
+	        });
+	    };
+      }
+    } else if (['templates', 'pads'].includes(key) && writables[key]) {
+      if (opts.length && opts[opts.length - 1]?.class !== 'divider') {
+	      opts.push({
+	        node: 'div',
+	        class: 'divider',
+	        label: vocabulary['or'].toUpperCase(),
+	      });
+	  };
+      if (writables[key]?.length) {
+        template_opts[key] = writables[key].map((d) => {
+          return {
+            label: `${d.title || d.name}${
+              d.ownername ? `<div class="sub">${d.ownername}</div>` : ''
+            }`,
+            value: d.id,
+            type: 'radio',
+          };
         });
-        if (writables[key]?.length) {
-          template_opts[key] = writables[key].map((d) => {
-            return {
-              label: `${d.title || d.name}${
-                d.ownername ? `<div class="sub">${d.ownername}</div>` : ''
-              }`,
-              value: d.id,
-              type: 'radio',
-            };
+
+        // let { write } = modules.find(d => d.type === key)?.rights
+        // if (write) {
+        // 	if (typeof write === 'object') write = write.blank
+        // 	if (write <= rights) {
+
+        if (rights[key] === 'write') {
+          template_opts[key].unshift({
+            label: vocabulary['blank pad'],
+            value: 'blank',
+            type: 'radio',
           });
+        }
+        // }
+        // }
 
-          // let { write } = modules.find(d => d.type === key)?.rights
-          // if (write) {
-          // 	if (typeof write === 'object') write = write.blank
-          // 	if (write <= rights) {
-
-          if (rights[key] === 'write') {
-            template_opts[key].unshift({
-              label: vocabulary['blank pad'],
-              value: 'blank',
-              type: 'radio',
-            });
-          }
-          // }
-          // }
-
-          opts.push({
-            node: 'select',
-            name: `template-${key}`,
-            label: `Create a new ${key.slice(0, -1)} (based on a template)`, // TO DO: TRANSLATE
-            options: template_opts[key],
-            resolve: function (node) {
-              if (node.value !== 'blank')
-                window.location.replace(
-                  `/${language}/contribute/${key.slice(0, -1)}?template=${
-                    node.value
-                  }&token=${encodeURIComponent(
-                    token,
-                  )}&action=publish_and_share`,
-                );
-              else
-                window.location.replace(
-                  `/${language}/contribute/${key.slice(
-                    0,
-                    -1,
-                  )}?token=${encodeURIComponent(
-                    token,
-                  )}&action=publish_and_share`,
-                );
-            },
-          });
-        } else {
-          opts.push({
-            node: 'button',
-            type: 'button',
-            label: `Create a new ${key.slice(0, -1)}`, // TO DO: TRANSLATE
-            resolve: (_) => {
+        opts.push({
+          node: 'select',
+          name: `template-${key}`,
+          label: `Create a new ${key.slice(0, -1)} (based on a template)`, // TO DO: TRANSLATE
+          options: template_opts[key],
+          resolve: function (node) {
+            if (node.value !== 'blank')
+              window.location.replace(
+                `/${language}/contribute/${key.slice(0, -1)}?template=${
+                  node.value
+                }&token=${encodeURIComponent(
+                  token,
+                )}&action=publish_and_share`,
+              );
+            else
               window.location.replace(
                 `/${language}/contribute/${key.slice(
                   0,
                   -1,
-                )}?token=${encodeURIComponent(token)}`,
+                )}?token=${encodeURIComponent(
+                  token,
+                )}&action=publish_and_share`,
               );
-            },
-          });
-        }
-      } else if (key === 'files' && writables[key]) {
-        opts.push({
-          node: 'div',
-          class: 'divider',
-          label: vocabulary['or'].toUpperCase(),
+          },
         });
+      } else {
         opts.push({
-          node: 'input',
-          type: 'file',
-          name: 'file',
-          accept: 'application/pdf,image/*,video/*,audio/*',
-          placeholder: `Upload a new ${key.slice(0, -1)}`, // TO DO: TRANSLATE
-          resolve: function (node) {
-            const formData = new FormData();
-            formData.append(this.name, node.files[0], node.files[0].name);
-
-            const form = {
-              action: '/upload/file',
-              method: 'POST',
-              data: formData,
-            };
-
-            // const storage_url = new URL(`<%- locals.metadata.site.app_storage %>`)
-            // storage_url.pathname = `${storage_url.pathname}/${'/uploads/save'}`.replace(/\/\//g, '/')
-
-            return new Promise(async (resolve) => {
-              const res = await uploadFile(form, language);
-              if (res.every((d) => d.status === 200)) {
-                const filepath = res[0].src;
-
-                const storage_url = new URL(
-                  d3.select('data[name="app_storage"]').node(),
-                );
-                storage_url.pathname =
-                  `${storage_url.pathname}/${filepath}`.replace(/\/\//g, '/');
-                const src = new URL(storage_url.pathname, storage_url.origin)
-                  .href;
-
-                resolve(
-                  window.location.replace(
-                    `/share/resource?token=${encodeURIComponent(
-                      token,
-                    )}&src=${src}`,
-                  ),
-                );
-              }
-            });
+          node: 'button',
+          type: 'button',
+          label: `Create a new ${key.slice(0, -1)}`, // TO DO: TRANSLATE
+          resolve: (_) => {
+            window.location.replace(
+              `/${language}/contribute/${key.slice(
+                0,
+                -1,
+              )}?token=${encodeURIComponent(token)}`,
+            );
           },
         });
       }
-      if (key !== Object.keys(readables)[Object.keys(readables).length - 1]) {
-        opts.push({
-          node: 'div',
-          class: 'divider',
-          label: vocabulary['or'].toUpperCase(),
-        });
-      }
+    } else if (key === 'files' && writables[key]) {
+      if (opts.length && opts[opts.length - 1]?.class !== 'divider') {
+	      opts.push({
+	        node: 'div',
+	        class: 'divider',
+	        label: vocabulary['or'].toUpperCase(),
+	      });
+	  };
+      opts.push({
+        node: 'input',
+        type: 'file',
+        name: 'file',
+        accept: 'application/pdf,image/*,video/*,audio/*',
+        placeholder: `Upload a new ${key.slice(0, -1)}`, // TO DO: TRANSLATE
+        resolve: function (node) {
+          const formData = new FormData();
+          formData.append(this.name, node.files[0], node.files[0].name);
+
+          const form = {
+            action: '/upload/file',
+            method: 'POST',
+            data: formData,
+          };
+
+          // const storage_url = new URL(`<%- locals.metadata.site.app_storage %>`)
+          // storage_url.pathname = `${storage_url.pathname}/${'/uploads/save'}`.replace(/\/\//g, '/')
+
+          return new Promise(async (resolve) => {
+            const res = await uploadFile(form, language);
+            if (res.every((d) => d.status === 200)) {
+              const filepath = res[0].src;
+
+              const storage_url = new URL(
+                d3.select('data[name="app_storage"]').node(),
+              );
+              storage_url.pathname =
+                `${storage_url.pathname}/${filepath}`.replace(/\/\//g, '/');
+              const src = new URL(storage_url.pathname, storage_url.origin)
+                .href;
+
+              resolve(
+                window.location.replace(
+                  `/share/resource?token=${encodeURIComponent(
+                    token,
+                  )}&src=${src}`,
+                ),
+              );
+            }
+          });
+        },
+      });
     }
   });
 

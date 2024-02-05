485e78ac16ca220d8362f4de0c448531662e0ec0
Josua Krause <josua.krause@gmail.com>
2024-02-05 18:00:12 -0500

    add ://

--- routes/helpers/index.js
+++ routes/helpers/index.js
@@ -277,9 +277,9 @@ exports.redirectBack = (req, res, baseIfEmpty = false) => {
 exports.redirectToLoginPlatform = (req, res, next) => {
   const originHost = req.get('host');
   const pathname = req?.originalUrl?.startsWith('/login')
-    ? new URL(`${req.protocol}${originHost}${req.originalUrl}`).searchParams.get('path')
+    ? new URL(`${req.protocol}://${originHost}${req.originalUrl}`).searchParams.get('path')
     : req.originalUrl;
-  const innerUrl = `${req.protocol}${originHost}${(pathname ?? '')}`;
+  const innerUrl = `${req.protocol}://${originHost}${(pathname ?? '')}`;
   const loginUrl = new URL(
     `${sso_app_url}/login?app=${app_title}&origin=${encodeURIComponent(innerUrl)}`,
   );

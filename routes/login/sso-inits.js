1a6ac5abc8f7efce1f6a1099b60117571d51f35e
Josua Krause <josua.krause@gmail.com>
2024-02-05 16:10:15 -0500

    lazily create msalClient

--- routes/login/sso-inits.js
+++ routes/login/sso-inits.js
@@ -1,7 +1,16 @@
 const msal = require('@azure/msal-node');
 const { msalConfig, sso_redirect_url } = include('config/')
-const msalClient = new msal.ConfidentialClientApplication(msalConfig);
 const { extractPathValue } = require('./device-info')
+
+let _msalClient = null;
+
+function getClient() {
+	if (!_msalClient) {
+		const msalClient = new msal.ConfidentialClientApplication(msalConfig);
+		_msalClient = msalClient;
+	}
+	return _msalClient;
+}
 
 module.exports = async (req, res, next) => {
 	const { referer } = req.headers || {}
@@ -21,6 +30,6 @@ module.exports = async (req, res, next) => {
 		state: encodedState
 	};
 
-	const authUrl = await msalClient.getAuthCodeUrl(authCodeUrlParameters);
+	const authUrl = await getClient().getAuthCodeUrl(authCodeUrlParameters);
 	res.redirect(authUrl);
 }

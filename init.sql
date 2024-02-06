6452a2964693cde81804ec9e1541f1f570c5e213
Josua Krause <josua.krause@gmail.com>
2024-02-05 19:07:11 -0500

    add login to extern_db

--- init.sql
+++ init.sql
@@ -166,7 +166,7 @@ CREATE TABLE mobilization_contributions (
 );
 
 CREATE TABLE extern_db (
-    id SERIAL PRIMARY KEY UNIQUE NOT NULL, 
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
     db VARCHAR(20) UNIQUE NOT NULL,
     url_prefix TEXT NOT NULL
 );
@@ -176,6 +176,7 @@ INSERT INTO extern_db (db, url_prefix) VALUES ('global', 'https://www.sdg-innova
 INSERT INTO extern_db (db, url_prefix) VALUES ('sm', 'https://solutions.sdg-innovation-commons.org/');
 INSERT INTO extern_db (db, url_prefix) VALUES ('blogs', 'https://blogs.sdg-innovation-commons.org/');
 INSERT INTO extern_db (db, url_prefix) VALUES ('consent', 'https://consent.sdg-innovation-commons.org/');
+INSERT INTO extern_db (db, url_prefix) VALUES ('login', 'https://login.sdg-innovation-commons.org/');
 
 CREATE TABLE pinboards (
     id SERIAL PRIMARY KEY UNIQUE NOT NULL,

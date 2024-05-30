1b50c2032ac302313ee98f73adf324e3e717ec55
Josua Krause <josua.krause@gmail.com>
2024-05-30 02:17:00 -0400

    better

--- public/js/contribute/pad/render.js
+++ public/js/contribute/pad/render.js
@@ -4,6 +4,8 @@ import {
   switchButtons,
   updateStatus,
 } from '/js/contribute/pad/save.js';
+// reactivate explorations later
+// import { getExploration } from '/js/contribute/pad/exploration.js';
 import { POST } from '/js/fetch.js';
 import { L, d3, uuidv4 } from '/js/globals.js';
 import { fixLabel, toggleClass, uploadFile } from '/js/main.js';
